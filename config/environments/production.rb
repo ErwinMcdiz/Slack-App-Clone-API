bc417ed8a223045eaa727558eaa310892c02e549
ErwinMcdiz <erwinmacahilo01@gmail.com>
2025-07-03 02:09:04 +0800

    small modif

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,8 +86,7 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.require_master_key = true
 end
-Rails.application.configure do
-  # ... other settings ...
+
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
